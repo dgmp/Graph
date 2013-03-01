d11b691fc04caf985c2a14d02826869a50120f09
karimfaiz <faizkarimo@gmail.com>
2013-03-01 10:38:56 +0100

    Update Graphs.java

--- Client/src/com/graphe/client/soap/Graphs.java
+++ Client/src/com/graphe/client/soap/Graphs.java
@@ -1,9 +1,3 @@
-/**
- * GraphsSOAP.java
- *
- * This file was auto-generated from WSDL
- * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
- */
 
 package com.graphe.client.soap;
 
